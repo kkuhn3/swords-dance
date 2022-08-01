4224b4acb04db513d9e9835ddb81779b14ee0208
kkuhn3 <karljkuhn@gmail.com>
2022-07-31 22:35:33 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# swords-dance
+
+A workout helper webpage.
+
+## Description
+
+A lot of services offer on/off for a set time workouts that you can do at home. So this will pick a at-home friendly workout and interval it for you. Future plans include adding a way for users to add workouts to the rotation and CSSing the page a bunch.
+
+## Getting Started
+
+### Executing program
+
+* Open index.html in a web browser
+
---
+++ index.js
@@ -0,0 +1,61 @@
+let loFlexes = [];
+let totalSeconds = 0;
+let upcomingFlex = "";
+
+function antiToAd() {
+	const checkboxes = document.querySelectorAll('input[type=checkbox]:checked')
+	for (var i = 0; i < checkboxes.length; i++) {
+		loFlexes.push(checkboxes[i].value)
+	}
+	anti.style.display="none";
+	post.style.display="block";
+	nextBreak();
+}
+
+function postToAnti() {
+	post.style.display="none";
+	anti.style.display="block";
+}
+
+function nextBreak() {
+	let setSeconds = 0;
+	timer.innerHTML = offTime.value;
+	upcomingFlex = loFlexes[Math.floor(Math.random() * loFlexes.length)];
+	content.innerHTML = "Next Up: " + upcomingFlex;
+	const interval = setInterval(function() {
+		setSeconds = setSeconds + 1;
+		totalSeconds = totalSeconds + 1;
+		
+		timer.innerHTML = offTime.value - setSeconds;
+
+		if(setSeconds >= offTime.value) {
+			clearInterval(interval);
+			nextFlex();
+		}
+		else if(totalSeconds >= totalTime.value * 60) {
+			clearInterval(interval);
+			postToAnti();
+		}
+	}, 1000);
+}
+
+function nextFlex() {
+	let setSeconds = 0;
+	timer.innerHTML = onTime.value;
+	content.innerHTML = upcomingFlex;
+	const interval = setInterval(function() {
+		setSeconds = setSeconds + 1;
+		totalSeconds = totalSeconds + 1;
+		
+		timer.innerHTML = onTime.value - setSeconds;
+
+		if(setSeconds >= onTime.value) {
+			clearInterval(interval);
+			nextBreak();
+		}
+		else if(totalSeconds >= totalTime.value * 60) {
+			clearInterval(interval);
+			postToAnti();
+		}
+	}, 1000);
+}
